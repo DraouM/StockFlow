6cfb3dd59b82aa759fc2170c71528bc1348611d1
Draou Mohammed Elamine <mohamedaminedraou@gmail.com>
2024-08-29 22:13:50 +0100

    create an api for hundling product model methods throw ipcRenderer

--- preload.js
+++ preload.js
@@ -2,3 +2,20 @@
 window.addEventListener("DOMContentLoaded", () => {
   console.log("Preload script loaded");
 });
+
+const { contextBridge, ipcRenderer } = require("electron");
+contextBridge.exposeInMainWorld("electronAPI", {
+  products: {
+    getAll: () => ipcRenderer.invoke("products-get-all"),
+    getById: (id) => ipcRenderer.invoke("products:getById", id),
+    create: (productData) => ipcRenderer.invoke("products:create", productData),
+    update: (id, productData) =>
+      ipcRenderer.invoke("products:update", id, productData),
+    delete: (id) => ipcRenderer.invoke("products:delete", id),
+  },
+  // orders: {
+  //   getRecent: () => ipcRenderer.invoke("orders:getRecent"),
+  //   // Add more order methods here
+  // },
+  // Add more modules here
+});
