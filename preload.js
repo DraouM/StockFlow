556e88b857b301e387962335db02e4d7f4ddb1ee
Draou Mohammed Elamine <mohamedaminedraou@gmail.com>
2024-09-05 06:11:21 +0100

    generate the context bridge functions for handling transactions

--- preload.js
+++ preload.js
@@ -13,9 +13,30 @@ contextBridge.exposeInMainWorld("electronAPI", {
       ipcRenderer.invoke("products:update", id, productData),
     delete: (id) => ipcRenderer.invoke("products:delete", id),
   },
-  // orders: {
-  //   getRecent: () => ipcRenderer.invoke("orders:getRecent"),
-  //   // Add more order methods here
-  // },
-  // Add more modules here
+  transactions: {
+    getAll: () => ipcRenderer.invoke("transactions-get-all"),
+    getById: (id) => ipcRenderer.invoke("transactions:getById", id),
+    create: (transactionData) =>
+      ipcRenderer.invoke("transactions:create", transactionData),
+    update: (id, transactionData) =>
+      ipcRenderer.invoke("transactions:update", id, transactionData),
+    delete: (id) => ipcRenderer.invoke("transactions:delete", id),
+    getByDateRange: (startDate, endDate, type) =>
+      ipcRenderer.invoke(
+        "transactions:getByDateRange",
+        startDate,
+        endDate,
+        type
+      ),
+    getTotalAmountByType: (type, startDate, endDate) =>
+      ipcRenderer.invoke(
+        "transactions:getTotalAmountByType",
+        type,
+        startDate,
+        endDate
+      ),
+    settle: (id) => ipcRenderer.invoke("transactions:settle", id),
+    getUnsettled: (partyId = null) =>
+      ipcRenderer.invoke("transactions:getUnsettled", partyId),
+  },
 });
