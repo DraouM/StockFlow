3f1301726c257be592ad47df530ac9479f1e140d
Draou Mohammed Elamine <mohamedaminedraou@gmail.com>
2024-11-07 11:49:48 +0100

    apply the common model to the create party model

--- pages/clients/client.js
+++ pages/clients/client.js
@@ -138,30 +138,30 @@ console.log("DONE");
 
 // Model
 
-document
-  .getElementById("display-model-btn")
-  .addEventListener("click", () => toggleModel());
+// document
+//   .getElementById("display-model-btn")
+//   .addEventListener("click", () => toggleModel());
 
-window.addEventListener("click", (event) => {
-  const model = document.querySelector(".model");
-  const overlay = document.getElementById("overlay"); // or use querySelector
+// window.addEventListener("click", (event) => {
+//   const model = document.querySelector(".model");
+//   const overlay = document.getElementById("overlay"); // or use querySelector
 
-  if (event.target === overlay && overlay.classList.contains("active")) {
-    toggleModel();
-  } else if (event.target === model && model.classList.contains("active")) {
-    //Prevent closing if click is inside model and model has active class.
-    //this part depends on whether you chose to add `active` class to model in toggle functions
-    event.stopPropagation(); // or event.preventDefault(); // or you can remove this completely depending on requirements
-    return;
-  }
-});
+//   if (event.target === overlay && overlay.classList.contains("active")) {
+//     toggleModel();
+//   } else if (event.target === model && model.classList.contains("active")) {
+//     //Prevent closing if click is inside model and model has active class.
+//     //this part depends on whether you chose to add `active` class to model in toggle functions
+//     event.stopPropagation(); // or event.preventDefault(); // or you can remove this completely depending on requirements
+//     return;
+//   }
+// });
 
-function toggleModel() {
-  const model = document.querySelector(".model");
-  const overlay = document.getElementById("overlay");
-  model.classList.toggle("active");
-  overlay.classList.toggle("active");
-}
+// function toggleModel() {
+//   const model = document.querySelector(".model");
+//   const overlay = document.getElementById("overlay");
+//   model.classList.toggle("active");
+//   overlay.classList.toggle("active");
+// }
 
 document.addEventListener("DOMContentLoaded", () => {
   const form1 = document.getElementById("form-1");
