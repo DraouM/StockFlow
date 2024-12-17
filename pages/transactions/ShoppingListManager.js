08405fa8d58ab9905311db30fd2872d4a865abc6
Draou Mohammed Elamine <mohamedaminedraou@gmail.com>
2024-12-17 17:26:21 +0100

    fix bug editing and deleting buttons

--- pages/transactions/ShoppingListManager.js
+++ pages/transactions/ShoppingListManager.js
@@ -3,8 +3,11 @@ import NotificationManager from "./notificationManager.js";
 import { UtilityHelpers } from "./selling/utilityHelpers.js";
 
 class ShoppingListManager {
-  constructor() {
+  constructor(formManager, modalManager) {
     this.shoppingList = []; // Initialize an empty shopping list
+    this.formManager = formManager; // Store the formManager instance
+    this.modalManager = modalManager; // Store the modalManager instance
+    this.tableBody = document.querySelector("#shopping-list tbody"); // Reference to the table body
   }
 
   // Method to add a new product to the shopping list
@@ -23,7 +26,8 @@ class ShoppingListManager {
   createNewItem(product) {
     const subTotal = product.subUnits * product.unitPrice;
     return {
-      id: Date.now().toString(), // Unique ID for the item
+      tempId: Date.now().toString(), // Unique ID for the item
+      productId: product.productId,
       productName: product.productName,
       quantity: product.quantity,
       unitPrice: product.unitPrice,
@@ -48,8 +52,10 @@ class ShoppingListManager {
 
   // Method to update an existing item in the shopping list
   updateProduct(itemId, updatedItem) {
+    console.log({ itemId, updatedItem });
+
     this.shoppingList = this.shoppingList.map((item) =>
-      item.id === itemId ? { ...item, ...updatedItem } : item
+      item.tempId === itemId ? { ...item, ...updatedItem } : item
     );
     this.renderList();
   }
@@ -70,19 +76,18 @@ class ShoppingListManager {
 
   // Method to render the shopping list in the UI
   renderList() {
-    const tableBody = document.querySelector("#shopping-list tbody");
-    tableBody.innerHTML = ""; // Clear the table body
+    this.tableBody.innerHTML = ""; // Clear the table body
 
     this.shoppingList.forEach((item, index) => {
       const newRow = this.createTableRow(item, index + 1);
-      tableBody.appendChild(newRow);
+      this.tableBody.appendChild(newRow);
     });
   }
 
   // Method to create a table row for the shopping list
   createTableRow(item, rowCount) {
     const newRow = document.createElement("tr");
-    newRow.dataset.id = item.id; // Store unique identifier
+    newRow.dataset.id = item.tempId; // Store unique identifier
 
     newRow.innerHTML = `
       <td><span class="number-circle">${rowCount}</span></td>
@@ -99,13 +104,50 @@ class ShoppingListManager {
         item.subTotal ? UtilityHelpers.formatNumber(item.subTotal) : "N/A"
       }</td>
        <td>
-        <button onclick="shoppingListManager.editItem (item.id)">Edit</button>
-        <button onclick="shoppingListManager.deleteProduct('${
-          item.id
-        }')">Delete</button>
+        <button class="edit-button" data-id="${item.tempId}">Edit</button>
+        <button class="delete-button" data-id="${item.tempId}">Delete</button>
       </td>
     `;
     return newRow;
+  }
+
+  // Method to handle edit button click
+  handleEditButtonClick(tempId) {
+    console.log("Shopping List ", this.shoppingList);
+
+    const itemToEdit = this.shoppingList.find((item) => item.tempId === tempId);
+    console.log({ itemToEdit, tempId });
+
+    if (itemToEdit) {
+      // Populate the form with the item's details for editing
+      this.formManager.populate("selling-form", itemToEdit);
+      // Optionally, you can set a flag to indicate that the form is in edit mode
+      document
+        .getElementById("selling-form")
+        .setAttribute("data-operation", "update");
+      document
+        .getElementById("selling-form")
+        .setAttribute("data-item-id", tempId);
+
+      // Open the product modal
+      this.modalManager.open("product-modal"); // Ensure modalManager is accessible
+    }
+  }
+
+  // Method to handle delete button click
+  deleteProduct(tempId) {
+    const indexToRemove = this.shoppingList.findIndex(
+      (item) => item.tempId === tempId
+    );
+    if (indexToRemove !== -1) {
+      const removedProduct = this.shoppingList[indexToRemove].productName;
+      this.shoppingList.splice(indexToRemove, 1);
+      this.renderList();
+      NotificationManager.showNotification(
+        `Removed ${removedProduct}`,
+        NotificationManager.NOTIFICATION_TYPES.INFO
+      );
+    }
   }
 
   // Method to populate the shopping list from fetched transaction details
